6a2456d0d16a7d2e5cbb0178374f105f9a422f12
JxfGoodLike <1873921663@qq.com>
2019-08-30 00:20:21 +0800

    Initial commit

---
+++ src/main/java/com/example/video/VideoApplication.java
@@ -0,0 +1,17 @@
+package com.example.video;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+//@MapperScan(basePackages="com.example.video.mapper")
+//@ComponentScan(basePackages= {"com.imooc", "org.n3r.idworker"})
+public class VideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoApplication.class, args);
+    }
+
+}
